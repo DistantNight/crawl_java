101f4f194d624077d865ad8a5f082ef1aeddb242
DistantNight <DistantNight@github.com>
2019-01-30 14:35:21 +0800

    Update getHtml.java
    1.  Modify the absolute path to relative path;
    2.  Add comment for main.

--- src/getHtml.java
+++ src/getHtml.java
@@ -15,33 +15,51 @@ import org.jsoup.select.Elements;
 
 
 public class getHtml {
+    // you can find this index in the api of dxy.com and I put an example of api in the project named api_example.json
+    // the following steps depend on the api and the source code of web
     private static int start_index = 1;
     private static int end_index = 106;
-    private static String file_path = "F:\\ideaProject\\SearchWeb\\data\\"; // the path to store the document
+    private static String file_path = ".\\data\\"; // the path to store the document
     public static void main(String[] args)
     {
+        // url use the interface of api to get the brief of diseases
         String url = "https://dxy.com/view/i/disease/list?section_group_name=buxian&page_index=";
+        /* url of each diseases' detailed introduction
+         * the index of diseases we can get in the above interface
+         * the number stored in the "date":{"items":["id"]}
+         */
         String disease_url = "https://dxy.com/disease/";
         try {
+            // traverse all the brief introduction of diseases, and get their id then stored these info in the file
             for(int temp = start_index; temp <= end_index; temp++){
+                // use connect to access the url
                 Response res = Jsoup.connect(url + Integer.toString(temp)).ignoreContentType(true).execute();
+                // get the whole html code of web, in this step we get the json file which stores the brief of diseases
                 String content_body = res.body();
+                // transform the string to a json object, in order to get the element conveniently
                 JSONObject json_string = JSONObject.fromObject(content_body);
                 System.out.println(json_string.toString());
                 if(json_string.has("data")){
+                    /* the function will return a string object
+                     * and the string is also a json object, so we need to transform it to a json object too
+                     */
                     JSONObject data_string = JSONObject.fromObject(json_string.getString("data"));
                     if(data_string.has("items")){
+                        // use this type to get the element of json array
                         JSONArray item_lists = data_string.getJSONArray("items");
                         for(int i = 0; i < item_lists.size(); i++){
                             JSONObject json_temp = JSONObject.fromObject(item_lists.getString(i));
                             if(json_temp.has("id")){
                                 try {
+                                    // use access intervals to avoid being identified as a robot by the web
                                     TimeUnit.MILLISECONDS.sleep(2000);
                                 }catch(InterruptedException e){
                                     e.printStackTrace();
                                 }
                                 String disease_id = json_temp.getString("id");
+                                // use the id we access the web of diseases
                                 Document doc = Jsoup.connect(disease_url + disease_id).get();
+                                // we can use 检查元素 to find the text we need in which <div> and then get them
                                 Elements disease_list = doc.getElementsByClass("disease-list");
                                 Document disease_doc = Jsoup.parse(disease_list.toString());
                                 try {
